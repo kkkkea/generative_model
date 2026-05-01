2b45eb599759bcacc1fc8f7d997912571652d166
kkkkea <447872721@qq.com>
2026-05-01 13:18:50 +0800

    update

--- dataset/aesthetic4k.py
+++ dataset/aesthetic4k.py
@@ -1,7 +1,9 @@
 from typing import Callable, Optional
+import warnings
 
 from torch.utils.data.dataset import Dataset
 from datasets import load_dataset, load_from_disk
+from PIL import Image
 from torchvision import transforms
 from torchvision.transforms import InterpolationMode
 
@@ -15,18 +17,30 @@ class Aesthetic4KDataset(Dataset):
         local_path: Optional[str] = None,
         transform: Optional[Callable] = None,
         val_limit: Optional[int] = None,
+        skip_small_images: bool = True,
+        max_image_pixels: Optional[int] = Image.MAX_IMAGE_PIXELS,
     ):
         self.split = split
+        self.lr_size = lr_size
+        self.gt_size = self.lr_size * downsample_ratio
+        self.max_image_pixels = max_image_pixels
+
         if local_path is not None:
             self.ds = load_from_disk(local_path)[split]
         else:
             self.ds = load_dataset("zhang0jhon/Aesthetic-4K", split=split)
-            
+
+        if skip_small_images or max_image_pixels is not None:
+            self.ds = self.ds.filter(
+                self._is_valid_image_size,
+                desc=(
+                    f"Filtering images smaller than {self.gt_size}x{self.gt_size} "
+                    "or too large"
+                ),
+            )
+
         if val_limit is not None and split != "train":
             self.ds = self.ds.select(range(min(val_limit, len(self.ds))))
-
-        self.lr_size = lr_size
-        self.gt_size = self.lr_size * downsample_ratio
 
         if transform is None:
             crop = (
@@ -43,6 +57,21 @@ class Aesthetic4KDataset(Dataset):
             interpolation=InterpolationMode.BICUBIC,
             antialias=True,
         )
+
+    def _is_valid_image_size(self, item) -> bool:
+        try:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", Image.DecompressionBombWarning)
+                image = item["image"]
+                width, height = image.size
+        except Image.DecompressionBombError:
+            return False
+
+        if width < self.gt_size or height < self.gt_size:
+            return False
+        if self.max_image_pixels is not None and width * height > self.max_image_pixels:
+            return False
+        return True
 
     def __len__(self):
         return len(self.ds)
